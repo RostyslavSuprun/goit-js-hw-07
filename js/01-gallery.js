7e78ca74092e631ad2cbc8e3f3012573f1ba76e1
RostyslavSuprun <kredlik62@gmail.com>
2022-05-29 23:05:14 +0300

    first try

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,11 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
+
+const markup = galleryItems.map((image) => `<div><img class= js-img src=${image.preview} alt = "${image.description}"></div>`)
+  .join("");
+
+// img.style.objectFit = contain;
+gallery.insertAdjacentHTML("beforeend", markup);
 
 console.log(galleryItems);
