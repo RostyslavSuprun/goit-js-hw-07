a8451b6be9cfb5e8e211864357dd2712109edf86
RostyslavSuprun <kredlik62@gmail.com>
2022-07-16 16:48:41 +0300

    hw #7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,10 +2,30 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const gallery = document.querySelector('.gallery');
 
-const markup = galleryItems.map((image) => `<div><img class= js-img src=${image.preview} alt = "${image.description}"></div>`)
+const markup = galleryItems.map(({ preview, description, original }) => `<div class= gallery__item><img class= gallery__image src=${preview} alt = "${description}"  data-source = ${original}></div>`)
   .join("");
 
-// img.style.objectFit = contain;
 gallery.insertAdjacentHTML("beforeend", markup);
 
-console.log(galleryItems);
+const imgEl = document.querySelectorAll('.gallery__image');
+const datasource = document.querySelector('[data-source]');
+
+function onclick(event) {
+  console.log(this.dataset.source)
+  basicLightbox.create(`<img width="1400" height="900" src='${this.dataset.source}'>`).show()
+
+}
+datasource.addEventListener("click", onclick);
+
+
+imgEl.forEach(img => {img.addEventListener("click", onclick)
+  }
+);
+
+document.addEventListener('keydown', (event) => {
+ const close = document.querySelector('.basicLightbox')
+  if (event.key === "Escape") {
+    close.remove()
+  }
+});
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+
+
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+const markup = galleryItems.map(({ preview, description, original }) =>`<li><a class="gallery__item" href="${original}"><img class= "gallery__image" src=${preview} alt = "${description}"></a></li>`)
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", markup);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+  captionPosition: "bottom",
+});
+
+// {/* <a class="gallery__item" href="large-image.jpg">
+//   <img class="gallery__image" src="small-image.jpg" alt="Image description" />
+// </a>
+//  */}
+
+// let gallery = new SimpleLightbox('.gallery a');
+// gallery.on('show.simplelightbox', function () {
+// 	// do something…
+// });
